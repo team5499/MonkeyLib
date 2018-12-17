c407ca768eff1d2f3af1d9b55cecf9f846eb2640
Ben Scholar <bens@digitalman.com>
2018-12-16 20:03:55 -0800

    added readme and worked on gradle stuff

---
+++ README.md
@@ -0,0 +1,2 @@
+# MonkeyLib
+MonkeyLib is team 5499's base kotlin code used in all of our robots. Mostly contains math and utilities
---
+++ src/test/kotlin/TestTest.kt
@@ -0,0 +1,10 @@
+
+import org.junit.jupiter.api.Test
+
+class TestTest {
+
+    @Test
+    fun test() {
+        println("test")
+    }
+}
