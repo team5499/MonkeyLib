e3b03a16611c7891b9ae08df76f2c73e21c1c2e9
Ben Scholar <bens@digitalman.com>
2018-12-17 15:47:07 -0800

    added dependecies

--- src/main/kotlin/frc/team5499/monkeyLib/auto/Action.kt
+++ src/main/kotlin/frc/team5499/monkeyLib/auto/Action.kt
@@ -1,27 +1,26 @@
 package frc.team5499.monkeyLib.auto
 
-// import edu.wpi.first.wpilibj.Timer
+import edu.wpi.first.wpilibj.Timer
 
 public open class Action(timeoutSeconds: Double) {
-    // private val mTimer: Timer
+    private val mTimer: Timer
     private val mTimeoutSeconds: Double
 
     init {
-        // mTimer = Timer()
+        mTimer = Timer()
         mTimeoutSeconds = timeoutSeconds
     }
 
     public open fun start() {
-        // mTimer.stop()
-        // mTimer.reset()
-        // mTimer.start()
+        mTimer.stop()
+        mTimer.reset()
+        mTimer.start()
     }
 
     public open fun update() {}
 
     protected fun timedOut(): Boolean {
-        // val t = mTimer.get()
-        val t = 0.0 // get rid of this when dependecy issue is fixed
+        val t = mTimer.get()
         return (t >= mTimeoutSeconds)
     }
 
