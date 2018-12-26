968f05b3816223fceaca37e692a44aac705c251f
Ben Scholar <bens@digitalman.com>
2018-12-26 13:42:20 -0800

    fixed package naming

--- src/main/kotlin/org/team5499/monkeyLib/hardware/LazyTalonSRX.kt
+++ src/main/kotlin/org/team5499/monkeyLib/hardware/LazyTalonSRX.kt
@@ -1,4 +1,4 @@
-package org.team5499.frc2018Kotlin.utils.hardware
+package org.team5499.monkeyLib.hardware
 
 import com.ctre.phoenix.motorcontrol.can.TalonSRX
 import com.ctre.phoenix.motorcontrol.ControlMode
