a67da0f8f478314ba2980a3d02196391a4c18f48
Ben Scholar <bens@digitalman.com>
2018-12-27 17:28:28 -0800

    initial

--- src/main/kotlin/org/team5499/monkeyLib/Controller.kt
+++ src/main/kotlin/org/team5499/monkeyLib/Controller.kt
@@ -1,7 +1,9 @@
 package org.team5499.monkeyLib
 
 public abstract class Controller {
-
+    /**
+    * function called to start the controller
+    */
     abstract fun start()
 
     abstract fun update()
