d9cb150f5723bacb9afee44158a3054aae46d166
Sam Bradley <sam54123@MacBook-Pro-3.local>
2019-01-18 18:01:22 -0800

    wrote documentation for auto stuff

--- src/main/kotlin/org/team5499/monkeyLib/auto/Action.kt
+++ src/main/kotlin/org/team5499/monkeyLib/auto/Action.kt
@@ -2,6 +2,10 @@ package org.team5499.monkeyLib.auto
 
 import edu.wpi.first.wpilibj.Timer
 
+/**
+ * A single auto command such as a motor movement, etc to be inserted into a Routine
+ * @param timeoutSeconds The amount of time to wait before aborting the action
+ */
 public open class Action(timeoutSeconds: Double) {
     private val mTimer: Timer
     private val mTimeoutSeconds: Double
@@ -11,22 +15,39 @@ public open class Action(timeoutSeconds: Double) {
         mTimeoutSeconds = timeoutSeconds
     }
 
+    /**
+     * Called when the action starts
+     */
     public open fun start() {
         mTimer.stop()
         mTimer.reset()
         mTimer.start()
     }
 
+    /**
+    * Called every tick while the action is active
+    */
     public open fun update() {}
 
+    /**
+    * Checks if this action has timed out.
+    * @return Is timed out?
+    */
     protected fun timedOut(): Boolean {
         val t = mTimer.get()
         return (t >= mTimeoutSeconds)
     }
 
+    /**
+     * Checks if this action is finished and ready to move on to the next one
+     * @return Is finished?
+    */
     public open fun next(): Boolean {
         return timedOut()
     }
 
+    /**
+     * Called when the auto command is finished in order clean up resources
+     */
     public open fun finish() {}
 }
--- src/main/kotlin/org/team5499/monkeyLib/auto/Routine.kt
+++ src/main/kotlin/org/team5499/monkeyLib/auto/Routine.kt
@@ -2,6 +2,12 @@ package org.team5499.monkeyLib.auto
 
 import org.team5499.monkeyLib.math.geometry.Rotation2d
 
+/**
+ * A list of Actions to run sequentially
+ * @param name The name of the routine
+ * @param startHeading The rotation the robot starts (used to callibrate gyro)
+ * @param actions The actions to run, in order of execution
+ */
 class Routine(name: String, startHeading: Rotation2d, vararg actions: Action) {
 
     val actions: Array<out Action>
@@ -27,10 +33,18 @@ class Routine(name: String, startHeading: Rotation2d, vararg actions: Action) {
         vararg actions: Action
     ): this(name, Rotation2d.fromDegrees(degreesHeading), *actions)
 
+    /**
+     * Returns the action the robot is currently executing
+     * @return Current action
+     */
     public fun getCurrentAction(): Action {
         return actions.get(stepNumber)
     }
 
+    /**
+     * Forces the robot to move to the next action
+     * @return Is the last step in the routine
+     */
     public fun advanceRoutine(): Boolean {
         if (isLastStep()) {
             return false
@@ -39,14 +53,24 @@ class Routine(name: String, startHeading: Rotation2d, vararg actions: Action) {
         return true
     }
 
+    /**
+     * Abort the current action and run the sequence from the given action index
+     */
     public fun setActionIndex(index: Int) {
         stepNumber = index
     }
 
+    /**
+     * Reset to the first action
+     */
     public fun reset() {
-        this.stepNumber = 0
+        stepNumber = 0
     }
 
+    /**
+     * Gets whether the sequence is currently on it's last step
+     * @return is last step?
+     */
     public fun isLastStep(): Boolean {
         return (stepNumber >= (actions.size - 1))
     }
