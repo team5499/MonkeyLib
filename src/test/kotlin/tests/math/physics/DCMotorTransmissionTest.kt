317a5764f1bb411dde06dc346aabf1c098bb4a63
Andy Cate <and164v@gmail.com>
2019-01-27 18:08:38 -0800

    Dev/dc motor transmission (#25)
    * Add translated transmission class
    
    * Add docs and unit tests
    
    * Update build.gradle

---
+++ src/test/kotlin/tests/math/physics/DCMotorTransmissionTest.kt
@@ -0,0 +1,34 @@
+package tests.math.physics
+
+import org.junit.Test
+import org.junit.Assert.assertEquals
+
+import org.team5499.monkeyLib.math.physics.DCMotorTransmission
+
+class DCMotorTransmissionTest {
+    val motor = DCMotorTransmission(1000.0, 0.5, 1.0)
+    val stallTorque = 5.5 // N•m
+    val freeSpeed = 11000.0 // rad/s
+    val epsilon = 0.1
+
+    @Test
+    fun stallTorqueTest() {
+        val calculatedStallTorque = motor.getTorqueForVoltage(0.0, 12.0)
+        println("Expected $stallTorque, but got $calculatedStallTorque.")
+        assertEquals(stallTorque, calculatedStallTorque, epsilon)
+    }
+
+    @Test
+    fun freeSpeedTest() {
+        val calculatedFreeSpeed = motor.freeSpeedAtVoltage(12.0)
+        println("Expected $freeSpeed, but got $calculatedFreeSpeed.")
+        assertEquals(freeSpeed, calculatedFreeSpeed, epsilon)
+    }
+
+    @Test
+    fun constantsTest() {
+        assertEquals(1000.0, motor.speedPerVolt, 0.0)
+        assertEquals(0.5, motor.torquePerVolt, 0.0)
+        assertEquals(1.0, motor.frictionVoltage, 0.0)
+    }
+}
