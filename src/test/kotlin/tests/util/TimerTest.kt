73c5b0845dfff4df2b568fdd498e5ba202fc9636
Ben Scholar <bens@digitalman.com>
2019-01-31 15:14:36 -0800

    added timer test

---
+++ src/test/kotlin/tests/util/TimerTest.kt
@@ -0,0 +1,30 @@
+package tests.utils
+
+import org.junit.Test
+import org.junit.Assert.assertEquals
+
+import org.team5499.monkeyLib.util.time.ITimer
+import org.team5499.monkeyLib.util.time.SystemTimer
+
+public class TimerTest {
+
+    private val kEpsilon = 1E6
+
+    @Test
+    fun timerSystemTest() {
+        val timer: ITimer = SystemTimer()
+        timer.start()
+        assertEquals(timer.get(), 0.0, kEpsilon)
+        Thread.sleep(2000L)
+        assertEquals(timer.get(), 2.0, kEpsilon)
+        timer.stop()
+        Thread.sleep(1000L)
+        timer.start()
+        Thread.sleep(1000L)
+        assertEquals(timer.get(), 3.0, kEpsilon)
+        timer.stop()
+        timer.reset()
+        timer.start()
+        assertEquals(timer.get(), 0.0, kEpsilon)
+    }
+}
