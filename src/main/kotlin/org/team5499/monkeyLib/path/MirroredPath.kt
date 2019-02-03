a124ad32bdb24b5d5d0ae3fa35930788ac84dd1f
Ben Scholar <bens@digitalman.com>
2019-02-03 00:26:34 -0800

    Added mirrored paths (#30)
    * added mirrored paths
    
    * documentation
    
    * Update build.gradle

---
+++ src/main/kotlin/org/team5499/monkeyLib/path/MirroredPath.kt
@@ -0,0 +1,22 @@
+package org.team5499.monkeyLib.path
+
+import org.team5499.monkeyLib.math.geometry.Pose2dWithCurvature
+
+/**
+* this class represents a path that can be mirrored over the centerline of the field
+* @property left is path on the left side of the field
+*/
+public class MirroredPath(left: Path) {
+
+    public val left: Path
+    public val right: Path
+
+    init {
+        this.left = left
+        val newPoints: MutableList<Pose2dWithCurvature> = left.points.toMutableList()
+        for (i in 0..newPoints.size - 1) {
+            newPoints.set(i, newPoints.get(i).mirror())
+        }
+        this.right = Path(newPoints, left.velocities, left.reversed)
+    }
+}
