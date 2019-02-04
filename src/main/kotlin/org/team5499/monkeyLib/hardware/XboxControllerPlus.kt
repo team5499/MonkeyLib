863d3f5570282f0a940db8cd16454a739fea7c54
Ben Scholar <bens@digitalman.com>
2019-02-03 19:10:51 -0800

    got it working finally

--- src/main/kotlin/org/team5499/monkeyLib/hardware/XboxControllerPlus.kt
+++ src/main/kotlin/org/team5499/monkeyLib/hardware/XboxControllerPlus.kt
@@ -1,38 +1,49 @@
 package org.team5499.monkeyLib.hardware
 
 import edu.wpi.first.wpilibj.XboxController
+import edu.wpi.first.wpilibj.Timer
 
-import org.team5499.monkeyLib.util.time.ITimer
-import org.team5499.monkeyLib.util.time.WPITimer
+// adapted from 1323
+public class XboxControllerPlus(portNumber: Int) : XboxController(portNumber) {
 
-public class XboxControllerPlus(portNumber: Int, timer: ITimer = WPITimer()) : XboxController(portNumber) {
+    public var rumbling = false
 
-    private val mRumbling = false
-    private val mTimer: ITimer
-
-    init {
-        mTimer = timer
+    public fun rumble(rumblesPerSecond: Double, numberOfSeconds: Double) {
+        if (!rumbling) {
+            val thread = RumbleThread(rumblesPerSecond, numberOfSeconds)
+            thread.start()
+        }
     }
 
-    public fun rumble() {
-    }
-
-    private class RumbleThread(rumblesPerSecond: Double, numberOfSeconds: Double) : Thread() {
+    private inner class RumbleThread(rumblesPerSecond: Double, numberOfSeconds: Double) : Thread() {
         private val mRumblesPerSecond: Double
         private val mInterval: Long
         private val mSeconds: Double
+        private var mStartTime: Double = 0.0
 
         init {
-            this.mRumblesPerSecond = rumblesPerSecond
+            mRumblesPerSecond = rumblesPerSecond
             mSeconds = numberOfSeconds
             mInterval = (1 / (rumblesPerSecond * 2.0) * 1000.0).toLong()
         }
 
         public override fun start() {
-            mRumbling = true
+            rumbling = true
+            mStartTime = Timer.getFPGATimestamp()
+            try {
+                while (Timer.getFPGATimestamp() - mStartTime < mSeconds) {
+                    setRumble(RumbleType.kLeftRumble, 1.0)
+                    setRumble(RumbleType.kRightRumble, 1.0)
+                    sleep(mInterval)
+                    setRumble(RumbleType.kLeftRumble, 0.0)
+                    setRumble(RumbleType.kRightRumble, 0.0)
+                    sleep(mInterval)
+                }
+            } catch (e: InterruptedException) {
+                rumbling = false
+                e.printStackTrace()
+            }
+            rumbling = false
         }
-    }
-
-    public override fun run() {
     }
 }
