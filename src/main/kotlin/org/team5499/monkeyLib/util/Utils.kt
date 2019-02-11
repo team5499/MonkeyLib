c8f7f0a4ecf02d2a3659f64e6f75111562f62a00
Ben Scholar <bens@digitalman.com>
2019-02-10 21:26:36 -0800

    added reduciton parameter to conversion methods, added docs, made function explicitly public

--- src/main/kotlin/org/team5499/monkeyLib/util/Utils.kt
+++ src/main/kotlin/org/team5499/monkeyLib/util/Utils.kt
@@ -3,48 +3,84 @@ package org.team5499.monkeyLib.util
 @SuppressWarnings("MagicNumber")
 object Utils {
 
-    fun limit(value: Double, limit: Double): Double {
+    public fun limit(value: Double, limit: Double): Double {
         return limit(value, -limit, limit)
     }
 
-    fun limit(value: Double, min: Double, max: Double): Double {
+    public fun limit(value: Double, min: Double, max: Double): Double {
         return Math.min(max, Math.max(min, value))
     }
 
-    fun interpolate(a: Double, b: Double, x: Double): Double {
+    public fun interpolate(a: Double, b: Double, x: Double): Double {
         val newX = limit(x, 0.0, 1.0)
         return a + (b - a) * newX
     }
 
-    fun inchesPerSecondToEncoderTicksPer100Ms(
+    /**
+    * @param encoderTicksPerRotation encoder ticks per 360 degrees of rotation
+    * @param circumference circumference of the object being rotated
+    * @param inchesPerSecond inches per second to be converted to ticks per 100 ms
+    * @param reduction reduction from the encoder shaft to the output shaft
+    */
+    public fun inchesPerSecondToEncoderTicksPer100Ms(
         encoderTicksPerRotation: Int,
-        wheelCircumference: Double,
-        ips: Double
+        circumference: Double,
+        inchesPerSecond: Double,
+        reduction: Double = 1.0
     ): Double {
-        return inchesToEncoderTicks(encoderTicksPerRotation, wheelCircumference, ips) / 10.0
+        return inchesToEncoderTicks(encoderTicksPerRotation, circumference, inchesPerSecond, reduction) / 10.0
     }
 
-    fun encoderTicksPer100MsToInchesPerSecond(
+    /**
+    * @param encoderTicksPerRotation encoder ticks per 360 degrees of rotation
+    * @param circumference circumference of the object being rotated
+    * @param encoderTicksPer100ms ticks per 100ms to be converted to inches per second
+    * @param reduction reduction from the encoder shaft to the output shaft
+    */
+    public fun encoderTicksPer100MsToInchesPerSecond(
         encoderTicksPerRotation: Int,
-        wheelCircumference: Double,
-        eps: Int
+        circumference: Double,
+        encoderTicksPer100ms: Int,
+        reduction: Double = 1.0
     ): Double {
-        return encoderTicksToInches(encoderTicksPerRotation, wheelCircumference, eps) * 10.0
+        return encoderTicksToInches(encoderTicksPerRotation, circumference, encoderTicksPer100ms, reduction) * 10.0
     }
 
-    fun inchesToEncoderTicks(encoderTicksPerRotation: Int, wheelCircumference: Double, inches: Double): Int {
-        return ((encoderTicksPerRotation / wheelCircumference) * inches).toInt()
+    /**
+    * @param encoderTicksPerRotation encoder ticks per 360 degrees of rotation
+    * @param circumference circumference of the object being rotated
+    * @param inches inches of travel to be converted to ticks of travel
+    * @param reduction reduction from the encoder shaft to the output shaft
+    */
+    public fun inchesToEncoderTicks(
+        encoderTicksPerRotation: Int,
+        circumference: Double,
+        inches: Double,
+        reduction: Double = 1.0
+    ): Int {
+        return (((encoderTicksPerRotation / circumference) * inches) / reduction).toInt()
     }
 
-    fun encoderTicksToInches(encoderTicksPerRotation: Int, wheelCircumference: Double, ticks: Int): Double {
-        return (wheelCircumference / encoderTicksPerRotation) * ticks
+    /**
+    * @param encoderTicksPerRotation encoder ticks per 360 degrees of rotation
+    * @param circumference circumference of the object being rotated
+    * @param ticks ticks of travel to be converted to inches of travel
+    * @param reduction reduction from the encoder shaft to the output shaft
+    */
+    public fun encoderTicksToInches(
+        encoderTicksPerRotation: Int,
+        circumference: Double,
+        ticks: Int,
+        reduction: Double = 1.0
+    ): Double {
+        return (circumference / encoderTicksPerRotation) * ticks * reduction
     }
 
-    fun talonAngleToDegrees(gyroTicksPerRotation: Int, ticks: Int): Double {
+    public fun talonAngleToDegrees(gyroTicksPerRotation: Int, ticks: Int): Double {
         return (360.0 / gyroTicksPerRotation) * ticks
     }
 
-    fun degreesToTalonAngle(gyroTicksPerRotation: Int, degrees: Double): Int {
+    public fun degreesToTalonAngle(gyroTicksPerRotation: Int, degrees: Double): Int {
         return ((gyroTicksPerRotation / 360.0) * degrees).toInt()
     }
 }
