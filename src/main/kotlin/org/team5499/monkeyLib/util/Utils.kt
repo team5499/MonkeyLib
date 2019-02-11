084eefbff6ab3189fc9e842c8b710ee3a01d07ce
Ben Scholar <bens@digitalman.com>
2019-02-10 21:31:57 -0800

    more docs

--- src/main/kotlin/org/team5499/monkeyLib/util/Utils.kt
+++ src/main/kotlin/org/team5499/monkeyLib/util/Utils.kt
@@ -3,14 +3,34 @@ package org.team5499.monkeyLib.util
 @SuppressWarnings("MagicNumber")
 object Utils {
 
+    /**
+    * method that limits input to a certain range.
+    * @param value value to be limited
+    * @param limit upper and lower bound of the value
+    * @return bounded value
+    */
     public fun limit(value: Double, limit: Double): Double {
         return limit(value, -limit, limit)
     }
 
+    /**
+    * method that limits input to a certain range.
+    * @param value value to be limited
+    * @param min lower bound of the value
+    * @param max upper bound of the value
+    * @return bounded value
+    */
     public fun limit(value: Double, min: Double, max: Double): Double {
         return Math.min(max, Math.max(min, value))
     }
 
+    /**
+    * method to interpolate between 2 doubles
+    * @param a first value
+    * @param b second value
+    * @param x value between the 2 values
+    * @return interpolated value
+    */
     public fun interpolate(a: Double, b: Double, x: Double): Double {
         val newX = limit(x, 0.0, 1.0)
         return a + (b - a) * newX
@@ -19,8 +39,9 @@ object Utils {
     /**
     * @param encoderTicksPerRotation encoder ticks per 360 degrees of rotation
     * @param circumference circumference of the object being rotated
-    * @param inchesPerSecond inches per second to be converted to ticks per 100 ms
+    * @param inchesPerSecond velocity to be converted
     * @param reduction reduction from the encoder shaft to the output shaft
+    * @return velocity in ticks per 100ms
     */
     public fun inchesPerSecondToEncoderTicksPer100Ms(
         encoderTicksPerRotation: Int,
@@ -34,7 +55,7 @@ object Utils {
     /**
     * @param encoderTicksPerRotation encoder ticks per 360 degrees of rotation
     * @param circumference circumference of the object being rotated
-    * @param encoderTicksPer100ms ticks per 100ms to be converted to inches per second
+    * @param encoderTicksPer100ms velocity to be converted
     * @param reduction reduction from the encoder shaft to the output shaft
     */
     public fun encoderTicksPer100MsToInchesPerSecond(
