f47cc0165135765c81dbfa847b4e0c80aa81e4e4
Ben Scholar <bens@digitalman.com>
2019-02-28 15:29:07 -0800

    added looper and loop

---
+++ src/main/kotlin/org/team5499/monkeyLib/loops/Looper.kt
@@ -0,0 +1,72 @@
+package org.team5499.monkeyLib.loops
+
+import edu.wpi.first.wpilibj.Notifier
+import edu.wpi.first.wpilibj.Timer
+
+import java.lang.Runnable
+
+public class Looper(period: Double = 0.005) {
+
+    private var mRunning: Boolean
+    private val mNotifier: Notifier
+    private val mLoops: MutableList<Loop> = mutableListOf()
+    private val mLock = Any()
+
+    private var mTimestamp = 0.0
+    private var mDt = 0.0
+
+    init {
+        mNotifier = Notifier(mRunnable)
+        mRunning = false
+    }
+
+    private val mRunnable: Runnable
+        get() = Runnable() {
+            synchronized(mLock) {
+                if (mRunning) {
+                    val now = Timer.getFPGATimestamp()
+                    for (loop in mLoops) {
+                        loop.onLoop(now)
+                    }
+
+                    mDt = now - mTimestamp
+                    mTimestamp = now
+                }
+            }
+        }
+
+    @Synchronized public fun register(loop: Loop) {
+        synchronized(mLock) {
+            mLoops.add(loop)
+        }
+    }
+
+    @Synchronized public fun start() {
+        if (!mRunning) {
+            println("Starting loops....")
+            synchronized(mLock) {
+                mTimestamp = Timer.getFPGATimestamp()
+                for (loop in mLoops) {
+                    loop.onStart(mTimestamp)
+                }
+                mRunning = true
+            }
+            mNotifier.startPeriodic(kPeriod)
+        }
+    }
+
+    @Synchronized public fun stop() {
+        if (mRunning) {
+            println("Stopping loops")
+            mNotifier.stop()
+            synchronized(mLock) {
+                mRunning = false
+                mTimestamp = Timer.getFPGATimestamp()
+                for (loop in mLoops) {
+                    println("Stopping loop: $loop")
+                    loop.onStop(mTimestamp)
+                }
+            }
+        }
+    }
+}
