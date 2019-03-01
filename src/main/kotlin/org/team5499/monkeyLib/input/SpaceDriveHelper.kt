113ca234383e4476926315c31a2ccd04896d36be
Ben Scholar <bens@digitalman.com>
2019-02-28 18:53:14 -0800

    added creep function

--- src/main/kotlin/org/team5499/monkeyLib/input/SpaceDriveHelper.kt
+++ src/main/kotlin/org/team5499/monkeyLib/input/SpaceDriveHelper.kt
@@ -1,25 +1,28 @@
 package org.team5499.monkeyLib.input
 
-public class SpaceDriveHelper(deadband: Double, turnMult: Double) : DriveHelper() {
+public class SpaceDriveHelper(deadband: Double, turnMult: Double, slowMult: Double) : DriveHelper() {
 
     private val mDeadband: Double
     private val mTurnMult: Double
+    private val mSlowMult: Double
 
     init {
         mDeadband = deadband
         mTurnMult = turnMult
+        mSlowMult = slowMult
     }
 
     public override fun calculateOutput(
         throttle: Double,
         wheel: Double,
         isQuickTurn: Boolean,
-        notUsed: Boolean
+        creep: Boolean
     ): DriveSignal {
         val newThottle = super.handleDeadband(throttle, mDeadband)
         var newWheel = super.handleDeadband(wheel, mDeadband)
         val mult = if (!isQuickTurn) mTurnMult else 1.0
         newWheel *= mult
-        return DriveSignal(newThottle + newWheel, newThottle - newWheel)
+        val slow = if (creep) mSlowMult else 1.0
+        return DriveSignal(slow * (newThottle + newWheel), slow * (newThottle - newWheel))
     }
 }
