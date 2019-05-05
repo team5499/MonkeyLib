73a7aeac5c01d7e186eb637d22ff7970f9efc89c
FelixMo42 <felix@tacocat.com>
2019-05-05 09:35:22 -0700

    added distance units

--- src/main/kotlin/org/team5419/fault/math/UnitConversion.kt
+++ src/main/kotlin/org/team5419/fault/math/UnitConversion.kt
@@ -1,14 +1,27 @@
-val CM: Double = 1.0
+/// DISTANCS UNITS ///
 
-val INCH: Double = 12.54
+val CENTIMETERS: Double = 1.0
+val DECIMETERS: Double = 10.0
+val METERS: Double = 100.0
+val KILOMETERS: Double = 1000.0 * METERS
 
+val INCHS: Double = 12.54
+val FEET: Double = 12 * INCHS
+val YARDS: Double = 3 * FEET
+val MILES: Double = 5280 * FEET
 
-data class Unit internal constructor(internal val value: Double) {
+// TIME UNITS //
+
+val SECOND: Double = 1.0
+
+/// UNIT CONVERSION ////
+
+data class UnitConverter internal constructor(internal val value: Double) {
 	fun to(conversion : Double): Double {
 		return value /  conversion
 	}
 }
 
-fun Double.from(conversion : Double) : Unit {
-	return Unit(this * conversion)
+fun Double.from(conversion : Double) : UnitConverter {
+	return UnitConverter(this * conversion)
 }
