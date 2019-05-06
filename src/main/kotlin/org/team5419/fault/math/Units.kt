ff13397b429e9dc85110fc0f8fb442e858b22826
FelixMo42 <felix@tacocat.com>
2019-05-05 17:30:56 -0700

    fixed import and added unit test

--- src/main/kotlin/org/team5419/fault/math/Units.kt
+++ src/main/kotlin/org/team5419/fault/math/Units.kt
@@ -1,31 +1,31 @@
-package org.team5419.fault.math
+package org.team5419.fault.math.units
 
-public object Units {
-	/// DISTANCS UNITS ///
+/// UNIT CONVERSION ///
 
-	public const val CENTIMETERS: Double = 1.0
-	public const val DECIMETERS: Double = 10.0
-	public const val METERS: Double = 100.0
-	public const val KILOMETERS: Double = 1000.0 * METERS
-
-	public const val INCHS: Double = 12.54
-	public const val FEET: Double = 12 * INCHS
-	public const val YARDS: Double = 3 * FEET
-	public const val MILES: Double = 5280 * FEET
-
-	// TIME UNITS //
-
-	public const val SECONDS: Double = 1.0
+public infix fun Double.from(conversion : Double) : UnitConverter {
+	return UnitConverter(this * conversion)
 }
 
-/// UNIT CONVERSION ////
-
 data class UnitConverter internal constructor(internal val value: Double) {
-	fun to(conversion : Double): Double {
-		return value /  conversion
+	public infix fun to(conversion : Double): Double {
+		return value / conversion
 	}
 }
 
-fun Double.from(conversion : Double) : UnitConverter {
-	return UnitConverter(this * conversion)
-}
+/// DISTANCS UNITS ///
+
+public const val CENTIMETERS: Double = 1.0
+public const val DECIMETERS: Double = 10.0
+public const val METERS: Double = 100.0
+public const val KILOMETERS: Double = 1000.0 * METERS
+
+public const val INCHS: Double = 2.54
+public const val FEET: Double = 12 * INCHS
+public const val YARDS: Double = 3 * FEET
+public const val MILES: Double = 5280 * FEET
+
+// TIME UNITS //
+
+public const val SECONDS: Double = 1.0
+public const val MINUTES: Double = 60.0
+public const val HOURS: Double = 60.0 * MINUTES
--- src/test/kotlin/tests/math/UnitsTests.kt
+++ src/test/kotlin/tests/math/UnitsTests.kt
@@ -1,13 +1,23 @@
 package tests.math
 
 import org.junit.Test
-import org.junit.Assert.assertEquals
+import org.junit.Assert.assertTrue
 
-import org.team5419.fault.math.Units
+import org.team5419.fault.math.units.*
 
 public class UnitConversionTests {
     @Test
     fun testInchToInch() {
-        assertEquals(1.0.from(Units.INCHS).to(Units.INCHS) , 1.0)
+        assertTrue(1.0 from INCHS to INCHS == 1.0)
+    }
+
+    @Test
+    fun testInchToCentimeter() {
+        assertTrue(124.0 from INCHS to CENTIMETERS == 314.96)
+    }
+
+    @Test
+    fun testMilesPerHouresToFeetPerSeconds() {
+        assertTrue(15.0 from MILES / HOURS to FEET / SECONDS == 22.0)
     }
 }
