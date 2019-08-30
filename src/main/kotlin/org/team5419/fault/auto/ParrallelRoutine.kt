622cc06360d0de020d78f6099071687cc26834e1
Nicholas <nachos.underwood@gmail.com>
2019-08-30 08:52:38 -0700

    done; no tests

--- src/main/kotlin/org/team5419/fault/auto/ParrallelRoutine.kt
+++ src/main/kotlin/org/team5419/fault/auto/ParrallelRoutine.kt
@@ -1,16 +1,27 @@
 
 package org.team5419.fault.auto
 
-public class ParrallelRoutine(name: String, startPose: Pose2d, vararg routines: Routines): Routine(name, startPose, routines) {
+public class ParrallelRoutine(name: String, startPose: Pose2d, vararg routines: Routines) {
+/*: Routine(name, startPose, routines) {*/
 
-  private val mActions: Array<out Routine>
+  private val mRoutines: Array<out Routine>
+  var stepNumber: Int
+      get() = field
+  val name: String
+      get() = field
+  val startPose: Pose2d
+      get() = field
 
+  @Suppress("SpreadOperator")
+  public constructor(name: String, vararg actions: Action) :
+      this(name, Pose2d(Vector2(0, 0), Rotation2d.fromDegrees(0.0)), *actions)
+
+  //inheriet init method
   init {
       this.stepNumber = 0
       this.name = name
       this.startPose = startPose
       this.routines = routines.copyOf()
-
   }
 
   override public fun getCurrentAction(): Array<out Action> {
@@ -21,10 +32,22 @@ public class ParrallelRoutine(name: String, startPose: Pose2d, vararg routines: 
     return r
   }
 
-  override public fun lastStep(): Boolean {
-    for(r: Routine in this.actions){
-      if()
+  override public fun advanceRoutine() {
+    if(!this.isLastStep()){
+      ste
+      for(r: Routine in this.routines){
+        r.advanceRoutine()
+      }
     }
+  }
+
+  override public fun isLastStep(): Boolean {
+    for(r: Routine in this.actions){
+      if(!r.lastStep()){
+        return false
+      }
+    }
+    return true
   }
 
 }
