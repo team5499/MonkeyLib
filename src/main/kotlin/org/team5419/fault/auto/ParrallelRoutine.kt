a8ee7f6e1f3658b8c8937f6f5461e7f34fd91df5
Nicholas <nachos.underwood@gmail.com>
2019-08-31 13:10:53 -0700

    done

--- src/main/kotlin/org/team5419/fault/auto/ParrallelRoutine.kt
+++ src/main/kotlin/org/team5419/fault/auto/ParrallelRoutine.kt
@@ -1,60 +1,63 @@
 
 package org.team5419.fault.auto
 
-public class ParrallelRoutine(name: String, startPose: Pose2d, vararg routines: Routines) {
+import org.team5419.fault.math.geometry.Rotation2d
+import org.team5419.fault.math.geometry.Vector2
+import org.team5419.fault.math.geometry.Pose2d
+
+public class ParrallelRoutine(name: String, startPose: Pose2d, vararg routines: Routine) {
 /*: Routine(name, startPose, routines) {*/
 
-  private val mRoutines: Array<out Routine>
-  var stepNumber: Int
-      get() = field
-  val name: String
-      get() = field
-  val startPose: Pose2d
-      get() = field
+    private val routines: Array<out Routine>
+    var stepNumber: Int
+        get() = field
+    val name: String
+        get() = field
+    val startPose: Pose2d
+        get() = field
 
-  @Suppress("SpreadOperator")
-  public constructor(name: String, vararg actions: Action) :
-      this(name, Pose2d(Vector2(0, 0), Rotation2d.fromDegrees(0.0)), *actions)
+    @Suppress("SpreadOperator")
+    public constructor(name: String, vararg routines: Routine) :
+        this(name, Pose2d(Vector2(0, 0), Rotation2d.fromDegrees(0.0)), *routines)
 
-  //inheriet init method
-  init {
-      this.stepNumber = 0
-      this.name = name
-      this.startPose = startPose
-      this.routines = routines.copyOf()
-  }
-
-  override public fun getCurrentAction(): Array<out Action> {
-    val actions: MutableList<out Action> = MutableList()
-    for(r: Routine in this.actions){
-     actions.add(r.getCurrentAction())
+    init {
+        this.stepNumber = 0
+        this.name = name
+        this.startPose = startPose
+        this.routines = routines.copyOf()
     }
-    return r
-  }
 
-  override public fun advanceRoutine() {
-    if(!this.isLastStep()){
-      ste
-      for(r: Routine in this.routines){
-        r.advanceRoutine()
-      }
+    // use an array instead?
+    // returns MutableList of all current Actions
+    public fun getCurrentActions(): MutableList<Action> {
+        val actions: MutableList<Action> = mutableListOf<Action>()
+        for (r: Routine in this.routines) actions.add(r.getCurrentAction())
+        return actions
     }
-  }
 
-  override public fun isLastStep(): Boolean {
-    for(r: Routine in this.actions){
-      if(!r.lastStep()){
-        return false
-      }
+    // advance all subroutines
+    public fun advanceRoutine() {
+        if (!this.isLastStep()) {
+            stepNumber++
+            for (r: Routine in this.routines) r.advanceRoutine()
+        }
     }
-    return true
-  }
 
-  override public fun rest() {
-    this.stepNumber = 0
-    for(r: Routine in this.routines){
-      r.reset()
+    // returns true if all subroutines are done
+    public fun isLastStep(): Boolean {
+        for (r: Routine in this.routines) {
+            if (!r.isLastStep()) {
+                return false
+            }
+        }
+        return true
     }
-  }
 
+    // resets all subroutines
+    public fun reset() {
+        this.stepNumber = 0
+        for (r: Routine in this.routines) {
+            r.reset()
+        }
+    }
 }
