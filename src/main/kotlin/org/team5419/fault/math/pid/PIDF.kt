538b95cf6203dc4af8724c92f039da1695463203
FelixMo42 <felix@tacocat.com>
2019-10-30 21:51:12 -0700

    added docs

--- src/main/kotlin/org/team5419/fault/math/pid/PIDF.kt
+++ src/main/kotlin/org/team5419/fault/math/pid/PIDF.kt
@@ -3,26 +3,55 @@ package org.team5419.fault.math.pid
 import edu.wpi.first.wpilibj.Timer
 
 public class PIDF {
-
+    /**
+     * The PIDF proportional constant.
+     */
     public var kP: Double
+
+    /**
+     * The PIDF integral constant.
+     */
     public var kI: Double
+
+    /**
+     * The PIDF derivative constant.
+     */
     public var kD: Double
+
+    /**
+     * The PIDF final constant.
+     */
     public var kF: Double
+
+    /**
+     * The max absolute value of the output.
+     */
     public var totalMax: Double
+
+    /**
+     * The set point the pid loop is targeting
+     */
     public var setpoint: Double
+
+    /**
+     * The value of the system. Used to get error when calculate is called.
+     */
     public var processVariable: Double
 
-    // number of iterations the accumulator keeps track of.
-    // 0 disables this (keeps track of all loops)
+    /**
+     * Number of iterations the accumulator keeps track of.
+     * 0 disables this (keeps track of all loops)
+     */
     public var integralZone: Int
-
-    public var inverted: Boolean
 
     private var lastError: Double
     private var accumulator: Double
     private var integralZoneBuffer: MutableList<Double>
     private val timer: Timer
 
+    /**
+     * Calculates the error based off of the current processVariable and setpoint
+     */
     val error: Double
         get() = (setpoint - processVariable)
 
@@ -38,25 +67,17 @@ public class PIDF {
         integralZone = 0
     }
 
-    public constructor(
-        kP: Double,
-        kI: Double,
-        kD: Double,
-        kF: Double,
-        inverted: Boolean = false,
-        totalMax: Double = 0.0
-    ) {
+    public constructor(kP: Double, kI: Double, kD: Double, kF: Double, totalMax: Double = 0.0) {
         this.kP = kP
         this.kI = kI
         this.kD = kD
         this.kF = kF
         this.totalMax = totalMax
-        this.inverted = inverted
     }
 
-    public constructor(): this(0.0, 0.0, 0.0, 0.0, false)
+    public constructor(): this(0.0, 0.0, 0.0, 0.0)
 
-    public fun updateAccumulator() {
+    private fun updateAccumulator() {
         if (integralZone == 0) {
             accumulator += error * timer.get()
         } else {
@@ -71,6 +92,14 @@ public class PIDF {
         }
     }
 
+    /**
+     * Calculates the current PIDF output based of the error.
+     * It also updates the accumulator and sets the last error.
+     *
+     * Make sure to update the processVariable before calling it.
+     *
+     * @return the PIDF output
+     */
     public fun calculate(): Double {
         var total = 0.0
 
@@ -103,7 +132,7 @@ public class PIDF {
                 total = totalMax
             }
 
-            if (total < -totalMax) {
+            if (total < /** > */ -totalMax) {
                 total = -totalMax
             }
         }
@@ -111,6 +140,10 @@ public class PIDF {
         return total
     }
 
+    /**
+     * Resets the pid loop timer, acumulator and setpoint.
+     * It dosent change the constants.
+     */
     public fun reset() {
         timer.stop()
         timer.reset()
