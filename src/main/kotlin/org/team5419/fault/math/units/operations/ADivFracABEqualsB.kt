ecdc203dde4c266369e8f9a5e30acd68f7c92986
Benjamin Scholar <bens@digitalman.com>
2019-11-07 23:04:18 -0500

    added operations

---
+++ src/main/kotlin/org/team5419/fault/math/units/operations/ADivFracABEqualsB.kt
@@ -0,0 +1,16 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ *
+ * Copyright 2019, Green Hope Falcons
+ */
+
+package org.ghrobotics.lib.mathematics.units.operations
+
+import org.team5419.fault.math.units.Frac
+import org.team5419.fault.math.units.SIKey
+import org.team5419.fault.math.units.SIUnit
+import org.team5419.fault.math.units.Unitless
+
+operator fun <A : SIKey, B : SIKey> SIUnit<A>.div(other: SIUnit<Frac<A, B>>) = SIUnit<B>(value.div(other.value))
